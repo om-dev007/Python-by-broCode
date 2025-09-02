312d2327fe79ac3f27009df4d8990effb2793039
om-dev007 <officialomnarayan@gmail.com>
2025-09-02 10:26:51 +0530

    Initial commit

---
+++ first.py
@@ -0,0 +1,49 @@
+from operator import truediv
+
+print("Hello OM")
+name = "Hello"
+
+#variable = A container for a value (string, integer, float, boolean)
+
+#                A varaible behaves as if it was the value it contains
+
+first_name = "BroCode"
+
+print(first_name)
+print(f"hello {first_name}")
+
+email = "brofake@gmail.com"
+
+print(f"Hi your email is {email}")
+
+# Integers
+
+age = 25
+
+print(f"Your age is {age}")
+
+queantity = 3
+
+print(f"your question is {queantity}")
+
+#float
+
+val = 6.7
+
+print(f"your val is {val}")
+
+thisTrue = True
+
+print(f"This is a boolean value {thisTrue}")
+
+if thisTrue:
+    print("This is a true value")
+else:
+    print("This is a false value")
+
+    for_Sale = True
+
+    if for_Sale:
+        print("THat item is for sale")
+    else:
+        print("that item is not for sale")
