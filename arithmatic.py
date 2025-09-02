9612edbd2531e35ca1ac8adebebab281eb1a5a42
om-dev007 <officialomnarayan@gmail.com>
2025-09-02 11:49:58 +0530

    exercise

---
+++ arithmatic.py
@@ -0,0 +1,26 @@
+friends = 5
+
+
+# friends = friends + 1
+# friends += 1
+# fiends = friends - 2
+# friends -= 2
+# friends = friends * 3
+# friends *= 3
+# friends = friends / 2
+#friends /= 2
+# friends = friends ** 2
+# friends **=2
+# remainder = friends % 2
+
+x = 3.14
+y = 4
+z = 5
+
+result = round(x)
+
+print(result)
+print(f"Absolute value of y is {abs(y)}")
+print(f"The power of y to the 2 is {pow(y, 2)}")
+print(f"The minimum value between {min(32,53,1)}")
+print(f"The maximum value between {max(32,3,534)}")
