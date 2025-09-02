82f52027bd7f0731a4e4df0d367ccc7fdb5649bc
om-dev007 <officialomnarayan@gmail.com>
2025-09-02 11:35:17 +0530

    exercise

---
+++ MadlibsGame.py
@@ -0,0 +1,14 @@
+#Madlibs Game
+# Word game where you can create a story
+# By filling in blanks with random words
+
+adjective1 = input("Enter an adjective: ")
+noun1 = input("enter a noun: ")
+adjective2 = input("Enter an another adjective: ")
+verb1 = input("Enter a verb: ")
+adjective3 = input("Enter 3rd another adjective: ")
+
+print(f"Today I went to a {adjective1} zoo.")
+print(f"In an exhibit, I saw a {noun1}")
+print(f"{noun1} was {adjective2} and {verb1}")
+print(f"I was {adjective3}")
