ac8b0b798c478709d6c04ffc7ba0903c5ffd6a53
om-dev007 <officialomnarayan@gmail.com>
2025-09-02 20:38:35 +0530

    exercise

---
+++ controlFlow.py
@@ -0,0 +1,34 @@
+# if = Do some code only if some condition is true
+# Else do something else
+
+# age = int(input("Enter age: "))
+
+# if age >= 19:
+#     print(f"Your age is {age}")
+# elif age < 0:
+#     print(f"You haven't entered {age}")
+# elif age > 100:
+#     print(f"Your {age} is greater than 100")
+# else:
+#     print(f"Your age is less than 19")
+
+# response = input("Would you kie food ? (Y/N): ")
+#
+# if response == "Y":
+#     print(f"Have some food!")
+# else:
+#     print("No food for you")
+
+# name = input("Enter name: ")
+#
+# if name == "":
+#     print(f"You didn't type your name!")
+# else:
+#     print(f"Hello {name}")
+
+online = False
+
+if online:
+    print("This item is for sale")
+else:
+    print("THis item is not for sale")
