4885bd21957047ed11ebc4107a656558cc6c52f1
om-dev007 <officialomnarayan@gmail.com>
2025-09-29 18:34:01 +0530

    new

--- weightCalculator.py
+++ weightCalculator.py
@@ -1,11 +1,3 @@
-# Pyhton Weight Calculator
+# Temperature Claculator
 
-weight = float(input("Weight: "))
-unit = input("Kilograms or pounds? (K or L): ")
-
-if unit == "K":
-    weight = weight * 2.205
-elif unit == "L":
-    weight = weight / 2.205
-else:
-    print(f"Your weight is {weight}")
+unit = 
