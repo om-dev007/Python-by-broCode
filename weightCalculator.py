0640d3489b4f0c152c9ed44e21e7ac0b9da32b6d
om-dev007 <officialomnarayan@gmail.com>
2025-09-29 18:53:45 +0530

    weigthCalc done

--- weightCalculator.py
+++ weightCalculator.py
@@ -1,2 +1,16 @@
 # Temperature Claculator
 
+weight = float(input("Enter your weight: "))
+
+unit = input("Kilograms or pounds? (K or L): ")
+
+if unit == "K":
+    weight = weight*2.205;
+    unit = "Lbs."
+elif unit == "L":
+    weight = weight/2.205;
+    unit = "Kgs."
+else:
+    print(f"{unit} was not valid")
+
+print(f"Your weight is : {round(weight, 1)} {unit}")
